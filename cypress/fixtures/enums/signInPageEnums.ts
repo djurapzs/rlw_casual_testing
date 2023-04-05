a7d72ea6567826d2a6b25aca63088deb57fb333d
Djuro Kurbatfinski <djurapez@gmail.com>
2023-04-05 17:21:39 +0200

    add sign in page enums

---
+++ cypress/fixtures/enums/signInPageEnums.ts
@@ -0,0 +1,16 @@
+export enum DataTestSelector {
+  signInError = "signin-error",
+  usernameField = "signin-username",
+  passwordField = "signin-password",
+  remmemberMeCheckBox = "signin-remember-me",
+  signInBtn = "signin-submit",
+  signUpLink = "signup",
+}
+
+export enum TextEnums {
+  signIn = "Sign in",
+  remmemberMe = "Remember me",
+  paswwordError = "Password must contain at least 4 characters",
+  signInAlertMsg = "Username or password is invalid",
+  signUpText = "Don't have an account? Sign Up",
+}
